eb8b2d4fc2a55a32a5aaacc58a4ef3a54b1e4dfb
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-02-29 17:32:36 -0800

    Create 1268B.cpp

---
+++ codeforces/1268B.cpp
@@ -0,0 +1,80 @@
+/*
+Domino : coloring the grid to black and white. the answer is min(|b|, |w|)
+*/
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include<iomanip>
+#include <sstream>
+#include <cmath>
+//#include "utils/haha.h"
+//#include "utils/max_flow.h"
+using namespace std;
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<string, int> PSI;
+typedef pair<int, PII> PIP;
+typedef long long ll;
+typedef pair<ll, ll> PLL;
+typedef pair<double, double> PDD;
+typedef pair<ll, PII> PLP;
+#define CLS(x, v) (memset((x), (v), sizeof((x))))
+const double pi = acos(-1);
+template<class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<class T>
+using max_heap = priority_queue<T>;
+const int mod = 1000000007;
+const int inf = 0x3fffffff;
+const int N = 1e5 + 10;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+struct PairHash {
+  template<typename T1, typename T2>
+  std::size_t operator()(const pair<T1, T2> &p) const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  }
+};
+
+void solve(int ncase) {
+  int n;
+  scanf("%d", &n);
+  ll f[2] = {0};
+  for(int i = 1; i <= n; i++) {
+    int x;
+    scanf("%d", &x);
+    f[i % 2] += x / 2;
+    f[(i%2) ^ 1] += x - x / 2;
+  }
+  cout << min(f[0], f[1]) << endl;
+}
+
+int main() {
+  //ios::sync_with_stdio(false);
+  cout << std::fixed;
+  cout << setprecision(9);
+#ifdef _zzz_
+  freopen("C:\\Users\\grain\\Desktop\\in.txt", "r", stdin);
+  //freopen("out.txt", "w", stdout);
+#endif
+  //precalc();
+  int T = 1;
+  //scanf("%d", &T);
+  //cin >> T;
+  int ncase = 0;
+  while (T--) {
+    solve(++ncase);
+  }
+}
