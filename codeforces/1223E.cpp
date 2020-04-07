baf82223f242f9c4d7313c9f846ac3e579db7d13
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-04-07 01:30:35 -0700

    Create 1223E.cpp
    Tree DP

---
+++ codeforces/1223E.cpp
@@ -0,0 +1,126 @@
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <fstream>
+//#include "utils/haha.h"
+//#include "utils/max_flow.h"
+using namespace std;
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<string, int> PSI;
+typedef pair<int, PII> PIP;
+typedef long long ll;
+typedef pair<int, ll> PIL;
+typedef pair<ll, ll> PLL;
+typedef pair<double, double> PDD;
+typedef pair<ll, PII> PLP;
+#define CLS(x, v) (memset((x), (v), sizeof((x))))
+const double pi = acos(-1);
+template<class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<class T>
+using max_heap = priority_queue<T>;
+const int mod = 1000000007;
+//const int mod = 998244353;
+const int inf = 0x3fffffff;
+//const int N = 2 * 1e5 + 10;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+struct PairHash {
+  template<typename T1, typename T2>
+  std::size_t operator()(const pair<T1, T2> &p) const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  }
+};
+
+int n, k;
+
+vector<vector<PII>> tree;
+
+PLL dfs(int r, int pre) {
+
+  vector<ll> sub;
+
+  ll full = 0;
+  ll not_full = 0;
+  ll base = 0;
+  for (auto p: tree[r]) {
+    if (p.first == pre) continue;
+    PLL sub_ret = dfs(p.first, r);
+    base += sub_ret.first;
+    sub.push_back(sub_ret.second + p.second - sub_ret.first);
+  }
+  if (sub.empty()) return PLL(0, 0);
+  sort(sub.begin(), sub.end());
+  ll add_on = 0;
+  for (int i = sub.size() - 1, j = 0; i >= 0 && j < k; i--, j++) {
+    if (sub[i] < 0) {
+      full = not_full = base + add_on;
+      break;
+    }
+    add_on += sub[i];
+    if (j == k - 1) {
+      not_full = base + add_on - sub[i];
+      full = base + add_on;
+    } else if (i == 0) {
+      full = not_full = base + add_on;
+    }
+
+  }
+  //cout << r << " " << pre << " : " << full << " " << not_full << endl;
+  return PLL(full, not_full);
+}
+
+void solve(int ncase) {
+  scanf("%d%d", &n, &k);
+  tree.assign(n + 1, vector<PII>());
+  for (int i = 0; i < n - 1; i++) {
+    int x, y, w;
+    scanf("%d%d%d", &x, &y, &w);
+    tree[x].push_back(PII(y, w));
+    tree[y].push_back(PII(x, w));
+  }
+  PLL ret = dfs(1, -1);
+  printf("%lld\n", ret.first);
+}
+
+void solve_all_cases() {
+  int T = 1;
+  scanf("%d", &T);
+  //cin >> T;
+  int ncase = 0;
+  while (T--) {
+    solve(++ncase);
+  }
+}
+
+int main() {
+  //ios::sync_with_stdio(false);
+  cout << std::fixed;
+  cout << setprecision(9);
+#ifdef _zzz_
+  freopen("C:\\Users\\grain\\Desktop\\in.txt", "r", stdin);
+  //freopen("C:\\Users\\grain\\Desktop\\out.txt", "w", stdout);
+  auto start_time = clock();
+#endif
+  solve_all_cases();
+  //test();
+#ifdef _zzz_
+  cout << (clock() - start_time) * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+#endif
+}
