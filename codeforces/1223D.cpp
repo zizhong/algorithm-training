bbac75c760a7c0a92c45d51a30a419da561ac988
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-04-07 02:28:11 -0700

    Create 1223D.cpp

---
+++ codeforces/1223D.cpp
@@ -0,0 +1,104 @@
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <fstream>
+//#include "utils/haha.h"
+//#include "utils/max_flow.h"
+using namespace std;
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<string, int> PSI;
+typedef pair<int, PII> PIP;
+typedef long long ll;
+typedef pair<int, ll> PIL;
+typedef pair<ll, ll> PLL;
+typedef pair<double, double> PDD;
+typedef pair<ll, PII> PLP;
+#define CLS(x, v) (memset((x), (v), sizeof((x))))
+const double pi = acos(-1);
+template<class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<class T>
+using max_heap = priority_queue<T>;
+const int mod = 1000000007;
+//const int mod = 998244353;
+const int inf = 0x3fffffff;
+//const int N = 2 * 1e5 + 10;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ 
+struct PairHash {
+  template<typename T1, typename T2>
+  std::size_t operator()(const pair<T1, T2> &p) const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  }
+};
+ 
+void solve(int ncase) {
+  int n;
+  scanf("%d", &n);
+  map<int, PII> m;
+  for (int i = 0; i < n; i++) {
+    int x;
+    scanf("%d", &x);
+    if (m.find(x) != m.end()) {
+      m[x].second = i;
+    } else {
+      m[x] = PII(i, i);
+    }
+  }
+  int seg = 0;
+  int last = n + 1;
+  int max_seg = 1;
+  for (auto p : m) {
+    if (p.second.first > last) {
+      seg++;
+      max_seg = max(max_seg, seg);
+      last = p.second.second;
+    } else {
+      seg = 1;
+      last = p.second.second;
+    }
+  }
+  printf("%d\n", m.size() - max_seg);
+}
+ 
+void solve_all_cases() {
+  int T = 1;
+  scanf("%d", &T);
+  //cin >> T;
+  int ncase = 0;
+  while (T--) {
+    solve(++ncase);
+  }
+}
+ 
+int main() {
+  //ios::sync_with_stdio(false);
+  cout << std::fixed;
+  cout << setprecision(9);
+#ifdef _zzz_
+  freopen("C:\\Users\\grain\\Desktop\\in.txt", "r", stdin);
+  //freopen("C:\\Users\\grain\\Desktop\\out.txt", "w", stdout);
+  auto start_time = clock();
+#endif
+  solve_all_cases();
+  //test();
+#ifdef _zzz_
+  cout << (clock() - start_time) * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+#endif
+}
