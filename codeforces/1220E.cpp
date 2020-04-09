4f2bf7dcfcb8f272fdbf7ab8ac733c8dfb5cdbf8
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-04-09 00:36:20 -0700

    Update 1220E.cpp

--- codeforces/1220E.cpp
+++ codeforces/1220E.cpp
@@ -45,19 +45,19 @@ const int mod = 1000000007;
 const int inf = 0x3fffffff;
 //const int N = 2 * 1e5 + 10;
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-
+ 
 struct PairHash {
   template<typename T1, typename T2>
   std::size_t operator()(const pair<T1, T2> &p) const {
     return hash<T1>()(p.first) ^ hash<T2>()(p.second);
   }
 };
-
+ 
 vector<int> w;
-
+ 
 int n, m;
 vector<vector<int>> g;
-
+ 
 #if 0
 // tarjan scc https://www.byvoid.com/zhs/blog/scc-tarjan
 vector<int> scc;
@@ -100,7 +100,7 @@ void tarjan_scc(int r) {
 }
 /// tarjan_scc END
 #endif
-
+ 
 // tarjan bridge https://oi-wiki.org/graph/cut/
 vector<int> dfn;
 vector<int> low;
@@ -116,7 +116,7 @@ void tarjan_bridge_init() {
   time_stamp = 0;
   bridge.clear();
 }
-
+ 
 void tarjan_bridge(int r, int pre) {
   father[r] = pre;
   dfn[r] = low[r] = time_stamp++;
@@ -134,10 +134,12 @@ void tarjan_bridge(int r, int pre) {
     }
   }
 }
-
+ 
 vector<vector<int>> tree;
 vector<ll> tree_w;
 vector<int> cc;
+vector<int> cnt;
+vector<int> choose;
 int cc_idx;
 void dfs_without_bridge(int r) {
   cc[r] = cc_idx;
@@ -151,6 +153,7 @@ void dfs_without_bridge(int r) {
 }
 void to_tree() {
   cc.assign(n + 1, -1);
+ 
   cc_idx = 0;
   for (int i = 1; i <= n; i++) {
     if (cc[i] == -1) {
@@ -158,55 +161,74 @@ void to_tree() {
       cc_idx++;
     }
   }
+  cnt.assign(cc_idx, 0);
   tree_w.assign(cc_idx, 0LL);
   for (int i = 1; i <= n; i++) {
     tree_w[cc[i]] += w[i];
+    cnt[cc[i]]++;
   }
   tree.assign(cc_idx, vector<int>());
+  choose.assign(cc_idx, false);
   for (auto p : bridge) {
     int u = cc[p.first];
     int v = cc[p.second];
     tree[u].push_back(v);
   }
 }
-
+ 
 ll ans;
-
-ll dfs(int r, int pre) {
-  vector<ll> ch;
+ll max_leaf = 0;
+ 
+bool dfs1(int r, int pre) {
+  if (cnt[r] > 1) choose[r] = true;
   for (auto u : tree[r]) {
     if (u == pre) continue;
-    ch.push_back(dfs(u, r));
+    if (dfs1(u, r)) choose[r] = true;
   }
-  sort(ch.begin(), ch.end(), greater<ll>());
-  ll tmp = tree_w[r];
-  for (int i = 0; i < 2 && i < ch.size(); i++) {
-    tmp += ch[i];
-  }
-  ans = max(ans, tmp);
-  return tree_w[r] + (ch.size() > 0 ? ch[0] : 0LL);
+ 
+  if (choose[r]) ans += tree_w[r];
+  return choose[r];
 }
+ 
+void dfs2(int r, int pre, ll v) {
+  bool leaf = true;
+  for (auto u : tree[r]) {
+    if (u == pre) continue;
+    dfs2(u, r, choose[u] ? 0 : v + tree_w[u]);
+  }
+  if (leaf) {
+    max_leaf = max(max_leaf, v);
+  }
+}
+ 
 void solve(int ncase) {
   scanf("%d%d", &n, &m);
   g.assign(n + 1, vector<int>());
   w.assign(n + 1, 0);
+  ll sum = 0;
   for (int i = 1; i <= n; i++) {
     scanf("%d", &w[i]);
+    sum += w[i];
   }
+ 
   for (int i = 0; i < m; i++) {
     int x, y;
     scanf("%d%d", &x, &y);
     g[x].push_back(y);
     g[y].push_back(x);
   }
+  int s;
+  scanf("%d", &s);
   tarjan_bridge_init();
   tarjan_bridge(1, -1);
   to_tree();
+  s = cc[s];
   ans = 0;
-  dfs(0, -1);
-  cout << ans << endl;
+  dfs1(s, -1);
+  dfs2(s, -1, choose[s] ? 0 : tree_w[s]);
+  cout << ans + max_leaf << endl;
 }
-
+ 
 void solve_all_cases() {
   int T = 1;
   //scanf("%d", &T);
@@ -216,7 +238,7 @@ void solve_all_cases() {
     solve(++ncase);
   }
 }
-
+ 
 int main() {
   //ios::sync_with_stdio(false);
   cout << std::fixed;
