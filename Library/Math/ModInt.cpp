f3ee4043264dfb6834664806c0cb657b5cd4e1d8
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-07-20 00:39:01 -0700

    Create ModInt.cpp

---
+++ Library/Math/ModInt.cpp
@@ -0,0 +1,39 @@
+// from https://codeforces.com/contest/1380/submission/86685201
+
+template<unsigned MOD_> struct ModInt {
+    static constexpr unsigned MOD = MOD_;
+    unsigned x;
+    void undef() { x = (unsigned)-1; }
+    bool isnan() const { return x == (unsigned)-1; }
+    inline int geti() const { return (int)x; }
+    ModInt() { x = 0; }
+    ModInt(const ModInt &y) { x = y.x; }
+    ModInt(int y) { if (y<0 || (int)MOD<=y) y %= (int)MOD; if (y<0) y += MOD; x=y; }
+    ModInt(unsigned y) { if (MOD<=y) x = y % MOD; else x = y; }
+    ModInt(long long y) { if (y<0 || MOD<=y) y %= MOD; if (y<0) y += MOD; x=y; }
+    ModInt(unsigned long long y) { if (MOD<=y) x = y % MOD; else x = y; }
+    ModInt &operator+=(const ModInt y) { if ((x += y.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(const ModInt y) { if ((x -= y.x) & (1u<<31)) x += MOD; return *this; }
+    ModInt &operator*=(const ModInt y) { x = (unsigned long long)x * y.x % MOD; return *this; }
+    ModInt &operator/=(const ModInt y) { x = (unsigned long long)x * y.inv().x % MOD; return *this; }
+    ModInt operator-() const { return (x ? MOD-x: 0); }
+ 
+    ModInt inv() const { return pow(MOD-2); }
+    ModInt pow(long long y) const {
+	ModInt b = *this, r = 1;
+	if (y < 0) { b = b.inv(); y = -y; }
+	for (; y; y>>=1) {
+	    if (y&1) r *= b;
+	    b *= b;
+	}
+	return r;
+    }
+ 
+    friend ModInt operator+(ModInt x, const ModInt y) { return x += y; }
+    friend ModInt operator-(ModInt x, const ModInt y) { return x -= y; }
+    friend ModInt operator*(ModInt x, const ModInt y) { return x *= y; }
+    friend ModInt operator/(ModInt x, const ModInt y) { return x *= y.inv(); }
+    friend bool operator<(const ModInt x, const ModInt y) { return x.x < y.x; }
+    friend bool operator==(const ModInt x, const ModInt y) { return x.x == y.x; }
+    friend bool operator!=(const ModInt x, const ModInt y) { return x.x != y.x; }
+};
