95bc05013d7bfcc7471b547da01831c07a81992d
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-11-25 00:48:25 -0800

    Create 304511H.cpp
    Matrix power to solve the recurrence relation

---
+++ codeforces/304511H.cpp
@@ -0,0 +1,249 @@
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <deque>
+#include <set>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <fstream>
+#include <bitset>
+#include <complex>
+#include <numeric>
+//#include "utils/haha.h"
+//#include "utils/max_flow.h"
+using namespace std;
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<string, int> PSI;
+typedef pair<int, PII> PIP;
+typedef long long ll;
+typedef pair<int, ll> PIL;
+typedef pair<ll, ll> PLL;
+typedef pair<double, double> PDD;
+typedef pair<ll, PII> PLP;
+#define CLS(x, v) (memset((x), (v), sizeof((x))))
+
+template<class TH>
+void _dbg(const char *sdbg, TH h) { cerr << sdbg << '=' << h << endl; }
+template<class TH, class... TA>
+void _dbg(const char *sdbg, TH h, TA... a) {
+  while (*sdbg != ',')cerr << *sdbg++;
+  cerr << '=' << h << ',';
+  _dbg(sdbg + 1, a...);
+}
+
+template<class T>
+ostream &operator<<(ostream &os, vector<T> V) {
+  os << "[";
+  for (auto vv : V) os << vv << ",";
+  return os << "]";
+}
+template<class L, class R>
+ostream &operator<<(ostream &os, pair<L, R> P) {
+  return os << "(" << P.first << "," << P.second << ")";
+}
+
+#ifdef _zzz_
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+
+template<class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<class T>
+using max_heap = priority_queue<T>;
+//const int N = 1e6 + 1e5 + 10;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+struct PairHash {
+  template<typename T1, typename T2>
+  std::size_t operator()(const pair<T1, T2> &p) const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  }
+};
+
+template<unsigned MOD_>
+struct ModInt {
+  static constexpr unsigned MOD = MOD_;
+  unsigned x;
+  void undef() { x = (unsigned) -1; }
+  bool isnan() const { return x == (unsigned) -1; }
+  inline int geti() const { return (int) x; }
+  ModInt() { x = 0; }
+  ModInt(const ModInt &y) { x = y.x; }
+  ModInt(int y) {
+    if (y < 0 || (int) MOD <= y) y %= (int) MOD;
+    if (y < 0) y += MOD;
+    x = y;
+  }
+  ModInt(unsigned y) { if (MOD <= y) x = y % MOD; else x = y; }
+  ModInt(long long y) {
+    if (y < 0 || MOD <= y) y %= MOD;
+    if (y < 0) y += MOD;
+    x = y;
+  }
+  ModInt(unsigned long long y) { if (MOD <= y) x = y % MOD; else x = y; }
+  ModInt &operator+=(const ModInt y) {
+    if ((x += y.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt &operator-=(const ModInt y) {
+    if ((x -= y.x) & (1u << 31)) x += MOD;
+    return *this;
+  }
+  ModInt &operator*=(const ModInt y) {
+    x = (unsigned long long) x * y.x % MOD;
+    return *this;
+  }
+  ModInt &operator/=(const ModInt y) {
+    x = (unsigned long long) x * y.inv().x % MOD;
+    return *this;
+  }
+  ModInt operator-() const { return (x ? MOD - x : 0); }
+
+  ModInt inv() const { return pow(MOD - 2); }
+  ModInt pow(long long y) const {
+    ModInt b = *this, r = 1;
+    if (y < 0) {
+      b = b.inv();
+      y = -y;
+    }
+    for (; y; y >>= 1) {
+      if (y & 1) r *= b;
+      b *= b;
+    }
+    return r;
+  }
+
+  friend ModInt operator+(ModInt x, const ModInt y) { return x += y; }
+  friend ModInt operator-(ModInt x, const ModInt y) { return x -= y; }
+  friend ModInt operator*(ModInt x, const ModInt y) { return x *= y; }
+  friend ModInt operator/(ModInt x, const ModInt y) { return x *= y.inv(); }
+  friend bool operator<(const ModInt x, const ModInt y) { return x.x < y.x; }
+  friend bool operator==(const ModInt x, const ModInt y) { return x.x == y.x; }
+  friend bool operator!=(const ModInt x, const ModInt y) { return x.x != y.x; }
+};
+const int mod = 1000000007;
+typedef ModInt<mod> mod_int;
+struct Matrix {
+  Matrix(int n = 27) {
+    this->n = n;
+    mat.assign(n, vector<ll>(n, 0));
+    identity();
+  }
+  void clear() {
+    mat.assign(n, vector<ll>(n, 0));
+  }
+  void identity() {
+    for (int i = 0; i < n; i++) mat[i][i] = 1;
+  }
+  void signal_char(int c) {
+    for (int i = 0; i < n; i++) mat[i][c] = 1;
+  }
+  Matrix operator+(const Matrix &rhs) const {
+    Matrix ret;
+    ret.clear();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        ret.mat[i][j] = (mat[i][j] +0LL+ rhs.mat[i][j]) % mod;
+      }
+    }
+    return ret;
+  }
+  Matrix operator*(const Matrix &rhs) const {
+    Matrix ret;
+    ret.clear();
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+          ret.mat[i][j] = (mat[i][k] *1LL* rhs.mat[k][j] + ret.mat[i][j]) % mod;
+        }
+      }
+    }
+    return ret;
+  }
+
+  Matrix operator^(ll y) const {
+     Matrix ret;
+     if (y == 0) return ret;
+     ret.mat = this->mat;
+     if (y == 1) return ret;
+     Matrix ret1 = (*this)^(y/2);
+     if (y &1) return ret1 * ret * ret1;
+     return ret1 * ret1;
+  }
+  vector<vector<ll>> mat;
+  
+  void init_recurrence(vector<int>& c) {
+    assert(n == c.size());
+    clear();
+    for(int i = 0; i < n - 1; i++) mat[i][i+1] = 1;
+    for(int i = 0; i < n; i++) mat[n-1][i] = c[i];
+  }
+  int n;
+};
+
+
+void solve(int ncase) {
+    int n , k;
+    cin >> n >> k;
+
+    Matrix mat(k);
+    vector<int> c(k,1);
+    mat.init_recurrence(c);
+    Matrix ret = mat^n;
+    //for(auto &v : ret.mat) {for(auto i : v) cout << i << " "; cout << endl;}
+    cout << ret.mat[k-1][0] << endl;
+}
+
+void solve_all_cases() {
+  int T = 1;
+  //scanf("%d", &T);
+  cin >> T;
+  int ncase = 0;
+  //pre_calc();
+  while (T--) {
+    solve(++ncase);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::fixed;
+  cout << setprecision(2);
+#ifdef _zzz_
+  //ios_base::sync_with_stdio(true);
+  freopen("C:\\Users\\grain\\Desktop\\in.txt", "r", stdin);
+  //auto x = freopen("C:\\Users\\grain\\Desktop\\out.txt", "w", stdout);
+  //cerr << x << " " << errno << endl;
+  auto start_time = clock();
+#endif
+  solve_all_cases();
+  //test();
+#ifdef _zzz_
+  cout << (clock() - start_time) * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+#endif
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+*/
