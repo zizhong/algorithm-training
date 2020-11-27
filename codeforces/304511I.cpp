d0e8222c75184be4eac137918f344076f58be8f1
Zizhong Zhang <zhangzizhong0828@gmail.com>
2020-11-27 01:14:02 -0800

    Create 304511I.cpp

---
+++ codeforces/304511I.cpp
@@ -0,0 +1,269 @@
+/**
+* Discretization + Euler Path on an undirected graph.
+*
+*/
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <deque>
+#include <set>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <fstream>
+#include <bitset>
+#include <complex>
+#include <numeric>
+//#include "utils/haha.h"
+//#include "utils/max_flow.h"
+using namespace std;
+typedef pair<int, int> PII;
+typedef pair<string, string> PSS;
+typedef pair<string, int> PSI;
+typedef pair<int, PII> PIP;
+typedef long long ll;
+typedef pair<int, ll> PIL;
+typedef pair<ll, ll> PLL;
+typedef pair<double, double> PDD;
+typedef pair<ll, PII> PLP;
+
+template<typename T>
+inline T read_by_char() {
+  T s = 0, f = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') f = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    s = (s << 3) + (s << 1) + ch - 48;
+    ch = getchar();
+  }
+  return s * f;
+}
+#define ri() read_by_char<int>()
+#define rl() read_by_char<ll>()
+
+#define CLS(x, v) (memset((x), (v), sizeof((x))))
+
+template<class TH>
+void _dbg(const char *sdbg, TH h) { cerr << sdbg << '=' << h << endl; }
+template<class TH, class... TA>
+void _dbg(const char *sdbg, TH h, TA... a) {
+  while (*sdbg != ',')cerr << *sdbg++;
+  cerr << '=' << h << ',';
+  _dbg(sdbg + 1, a...);
+}
+
+template<class T>
+ostream &operator<<(ostream &os, vector<T> V) {
+  os << "[";
+  for (auto vv : V) os << vv << ",";
+  return os << "]";
+}
+template<class L, class R>
+ostream &operator<<(ostream &os, pair<L, R> P) {
+  return os << "(" << P.first << "," << P.second << ")";
+}
+
+#ifdef _zzz_
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+
+template<class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<class T>
+using max_heap = priority_queue<T>;
+//const int N = 1e6 + 1e5 + 10;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+struct PairHash {
+  template<typename T1, typename T2>
+  std::size_t operator()(const pair<T1, T2> &p) const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  }
+};
+
+template<unsigned MOD_>
+struct ModInt {
+  static constexpr unsigned MOD = MOD_;
+  unsigned x;
+  void undef() { x = (unsigned) -1; }
+  bool isnan() const { return x == (unsigned) -1; }
+  inline int geti() const { return (int) x; }
+  ModInt() { x = 0; }
+  ModInt(const ModInt &y) { x = y.x; }
+  ModInt(int y) {
+    if (y < 0 || (int) MOD <= y) y %= (int) MOD;
+    if (y < 0) y += MOD;
+    x = y;
+  }
+  ModInt(unsigned y) { if (MOD <= y) x = y % MOD; else x = y; }
+  ModInt(long long y) {
+    if (y < 0 || MOD <= y) y %= MOD;
+    if (y < 0) y += MOD;
+    x = y;
+  }
+  ModInt(unsigned long long y) { if (MOD <= y) x = y % MOD; else x = y; }
+  ModInt &operator+=(const ModInt y) {
+    if ((x += y.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  ModInt &operator-=(const ModInt y) {
+    if ((x -= y.x) & (1u << 31)) x += MOD;
+    return *this;
+  }
+  ModInt &operator*=(const ModInt y) {
+    x = (unsigned long long) x * y.x % MOD;
+    return *this;
+  }
+  ModInt &operator/=(const ModInt y) {
+    x = (unsigned long long) x * y.inv().x % MOD;
+    return *this;
+  }
+  ModInt operator-() const { return (x ? MOD - x : 0); }
+
+  ModInt inv() const { return pow(MOD - 2); }
+  ModInt pow(long long y) const {
+    ModInt b = *this, r = 1;
+    if (y < 0) {
+      b = b.inv();
+      y = -y;
+    }
+    for (; y; y >>= 1) {
+      if (y & 1) r *= b;
+      b *= b;
+    }
+    return r;
+  }
+
+  friend ModInt operator+(ModInt x, const ModInt y) { return x += y; }
+  friend ModInt operator-(ModInt x, const ModInt y) { return x -= y; }
+  friend ModInt operator*(ModInt x, const ModInt y) { return x *= y; }
+  friend ModInt operator/(ModInt x, const ModInt y) { return x *= y.inv(); }
+  friend bool operator<(const ModInt x, const ModInt y) { return x.x < y.x; }
+  friend bool operator==(const ModInt x, const ModInt y) { return x.x == y.x; }
+  friend bool operator!=(const ModInt x, const ModInt y) { return x.x != y.x; }
+};
+const int mod = (1e9) + 7;
+typedef ModInt<mod> mod_int;
+struct Edge {
+  int to;
+  bool exist;
+  int rev_idx;
+  Edge(int t, int rev_idx_) : to(t), rev_idx(rev_idx_), exist(true) {}
+  bool operator<(const Edge &rhs) const {
+    return to < rhs.to;
+  }
+};
+
+
+vector<vector<Edge>> graph;
+
+void dfs(int u, vector<int> &euler_path) {
+  for (auto &e : graph[u]) {
+    if (!e.exist) continue;
+    e.exist = false;
+    graph[e.to][e.rev_idx].exist = false;
+    dfs(e.to, euler_path);
+  }
+  euler_path.push_back(u);
+}
+void solve(int ncase) {
+  int m = ri();
+  vector<PII> edge(m);
+  int idx = 0;
+  unordered_map<int, int> x2idx;
+  unordered_map<int, int> idx2x;
+  for (int i = 0; i < m; i++) {
+    edge[i].first = ri();
+    edge[i].second = ri();
+    if (x2idx.find(edge[i].first) == x2idx.end()) {
+      idx2x[idx] = edge[i].first;
+      x2idx[edge[i].first] = idx++;
+    }
+    if (x2idx.find(edge[i].second) == x2idx.end()) {
+      idx2x[idx] = edge[i].second;
+      x2idx[edge[i].second] = idx++;
+    }
+  }
+  graph.assign(idx, vector<Edge>());
+  vector<int> degree(idx);
+  for (int i = 0; i < m; i++) {
+    int u = x2idx[edge[i].first];
+    int v = x2idx[edge[i].second];
+    degree[u]++;
+    degree[v]++;
+    graph[u].push_back(Edge(v, graph[v].size()));
+    graph[v].push_back(Edge(u, (int) graph[u].size() - 1));
+  }
+  int start = -1;
+  for (int i = 0; i < idx; i++) {
+    if (degree[i] & 1) {
+      start = i;
+      break;
+    }
+  }
+  if (start < -1) start = 0;
+  vector<int> euler_path;
+  dfs(start, euler_path);
+  if (euler_path.size() != m + 1) {
+    cout << -1 << endl;
+    return;
+  }
+  cout << euler_path.size() << endl;
+  for (auto i : euler_path) cout << idx2x[i] << " ";
+  cout << endl;
+}
+
+void solve_all_cases() {
+  int T = 1;
+  //scanf("%d", &T);
+  //cin >> T;
+  int ncase = 0;
+  //pre_calc();
+  while (T--) {
+    solve(++ncase);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cout << std::fixed;
+  cout << setprecision(2);
+#ifdef _zzz_
+  //ios_base::sync_with_stdio(true);
+  freopen("C:\\Users\\grain\\Desktop\\in.txt", "r", stdin);
+  //auto x = freopen("C:\\Users\\grain\\Desktop\\out.txt", "w", stdout);
+  //cerr << x << " " << errno << endl;
+  auto start_time = clock();
+#endif
+  solve_all_cases();
+  //test();
+#ifdef _zzz_
+  cout << (clock() - start_time) * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+#endif
+}
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+*/
