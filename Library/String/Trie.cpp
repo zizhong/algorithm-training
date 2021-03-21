f4c10dafcba6dfab8f62b1667f43a6301e14c641
Zizhong Zhang <zhangzizhong0828@gmail.com>
2021-03-20 20:38:50 -0700

    Update Trie.cpp

--- Library/String/Trie.cpp
+++ Library/String/Trie.cpp
@@ -1,40 +1,47 @@
 struct TrieNode {
-    static const int N = 26;
-    TrieNode* next[N];
-    bool is_end = false;
-    TrieNode() {
-        fill(next, next + N, nullptr);
-        is_end = false;
-    }
-    int char2index(const char c) {
-        return c - 'a';
-    }
-    TrieNode*& getNext(const char c) {
-        return next[char2index(c)];
-    }
-    ~TrieNode() {
-        for(int i = 0; i < N; i++) delete next[i];
-    }
+  static const int N = 26;
+  TrieNode *next[N];
+  bool is_end = false;
+  TrieNode() {
+    init();
+  }
+  void init() {
+    fill(next, next + N, nullptr);
+    is_end = false;
+  }
+  int char2index(const char c) {
+    return c - 'a';
+  }
+  TrieNode *&getNext(const char c) {
+    return next[char2index(c)];
+  }
+  ~TrieNode() {
+  }
 };
-struct Trie {
-    
-    TrieNode* root;
-    Trie() {
-        root = new TrieNode();
-    }
-    ~Trie() {
-        delete root;
-    }
+TrieNode node[(int) 1e6];
+int node_idx;
 
-    void insert(const string& str) {
-        auto p = root;
-        for(char c : str) {
-            auto& nextp = p->getNext(c);
-            if (nullptr == nextp) {
-                nextp = new TrieNode();
-            }
-            p = nextp;
-        }
-        p->is_end = true;
+struct Trie {
+
+  TrieNode *root;
+  Trie() {
+    node_idx = 0;
+    root = alloc();
+  }
+  TrieNode *alloc() {
+    node[node_idx].clear();
+    return &node[node_idx++];
+  }
+
+  void insert(const string &str) {
+    auto p = root;
+    for (char c : str) {
+      auto &nextp = p->getNext(c);
+      if (nullptr == nextp) {
+        nextp = new TrieNode();
+      }
+      p = nextp;
     }
+    p->is_end = true;
+  }
 };
