16e7e5b94aca9b2629f5b6cad37d2b1d1a12c191
Zizhong Zhang <zhangzizhong0828@gmail.com>
2025-01-16 00:43:29 -0800

    Update CompositedBarrier.cpp

--- Library/Concurrent/CompositedBarrier.cpp
+++ Library/Concurrent/CompositedBarrier.cpp
@@ -1,68 +1,73 @@
+/*
+*
+1. ++wait on enter and --pending on exit
+  a) As to_pending will move wait to pending
+2. Call the callback while holding lock
+3. The wait logic is that only if
+  a) Currently this index is pending
+  b) OR there is no pending and there is a group ready
+*/
+struct CompositeBarrier {
+    vector<int> need;
+    vector<int> pending;
+    vector<int> wait;
+    mutex m;
+    condition_variable cv;
+    CompositeBarrier(const vector<int>& need_) : need(need_) {
+        pending.resize(need.size());
+        wait.resize(need.size());
+    };
+    bool ready() {
+        for (int j = 0; j < need.size(); j++) {
+            if (need[j] > wait[j])
+                return false;
+        }
+        return true;
+    }
 
-class CompositedBarrier {
- private:
-  mutex m_mutex;
-  condition_variable m_cv;
-  vector<int> m_cnt;
-  vector<int> m_inuse;
-  vector<int> m_need;
-  int m_group;
-  bool enough() {
-    for (int i = 0; i < m_group; i++) {
-      if (m_cnt[i] < m_need[i]) return false;
+    void to_pending() {
+        for (int j = 0; j < need.size(); j++) {
+            pending[j] += need[j];
+            wait[j] -= need[j];
+        }
     }
-    return true;
-  }
-  bool in_pending() {
-    for (int i = 0; i < m_group; i++) {
-      if (m_inuse[i] > 0) return true;
+    bool no_pending() {
+        for (auto x : pending)
+            if (x)
+                return false;
+        return true;
     }
-    return false;
-  }
-  void to_pending() {
-    for (int i = 0; i < m_group; i++) {
-      m_inuse[i] = m_need[i];
-      m_cnt[i] -= m_need[i];
+    void handle(int i, function<void()> callback) {
+        {
+            unique_lock<mutex> lk(m);
+            wait[i]++;
+            cv.wait(lk,
+                    [&]() { return pending[i] > 0 || no_pending() && ready(); });
+            if (pending[i] == 0) {
+                to_pending();
+            }
+            pending[i]--;
+           callback();
+        }
+        cv.notify_all();
     }
-  }
- public:
-  CompositedBarrier(const vector<int> &need_) {
-    m_need = need_;
-    m_group = need_.size();
-    m_cnt.assign(need_.size(), 0);
-    m_inuse.assign(need_.size(), 0);
-  }
-  void handle(int x) {
-    unique_lock<mutex> lk(m_mutex);
-    m_cv.wait(lk, [&] { return !in_pending(); });
-    m_cnt[x]++;
-    m_cv.wait(lk, [&] { return enough() || m_inuse[x] > 0; });
-    if (m_inuse[x] > 0) {
-      m_inuse[x]--;
-      m_cv.notify_all();
-      return;
-    }
-    to_pending();
-    m_inuse[x]--;
-    m_cv.notify_all();
-  };
 };
+
 class H2O {
+    CompositeBarrier cb;
+
 public:
-    CompositedBarrier cb;
-    H2O() : cb({2, 1}) {
-        
-    }
+    H2O() : cb(CompositeBarrier({2, 1})) {}
 
     void hydrogen(function<void()> releaseHydrogen) {
-        cb.handle(0);
+        cb.handle(0, releaseHydrogen);
         // releaseHydrogen() outputs "H". Do not change or remove this line.
-        releaseHydrogen();
+        // releaseHydrogen();
     }
 
     void oxygen(function<void()> releaseOxygen) {
-        cb.handle(1);
+        cb.handle(1, releaseOxygen);
         // releaseOxygen() outputs "O". Do not change or remove this line.
-        releaseOxygen();
+        // releaseOxygen();
     }
 };
